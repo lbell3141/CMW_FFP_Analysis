f2b914dea1121269bb78288d329ea33f6b2a3608
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-04-01 17:04:57 -0700

    initial commit; outlined

---
+++ Scripts/WD_characterize.R
@@ -0,0 +1,10 @@
+#Wind direction characterization
+
+#Diurnal Variation
+
+#Annual Variation
+
+#GPP, NEE, and LE by WD
+
+
+#table of average driver values (winter, pre, post)- PPFD, RH, TA, VPD, WS, RECO, GPP, NEE
