ed1a023ab16f0b13aeea04d24860031fc35e197c
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-05-13 16:49:26 -0700

    prep for splitting 170 to 220 for ffp

---
+++ Scripts/format_FFP_8WD_csv.R
@@ -0,0 +1,57 @@
+#split WD into 8 sections for FFP analysis
+
+
+library(lubridate)
+library(dplyr)
+
+#output paths
+pathtoFullDaytimeOutput <- "./Data/FFPCalculator_Inputs/CMW_Full_DT.csv"
+pathtoNWOutput <- "./Data/FFPCalculator_Inputs/CMW_NW_DT.csv"
+pathtoSEOutput <- "./Data/FFPCalculator_Inputs/CMW_SE_DT.csv"
+
+#Load data; set parameters
+dat_file <- read.csv("./Data/AMF_US-CMW_BASE_HH_2-5.csv",  na.strings = "-9999", header = TRUE, sep = ",", skip = 2)
+dat_file$TIMESTAMP_START <- ymd_hm(as.character(dat_file$TIMESTAMP_START))
+
+meas_h <- 14
+d <- (2/3) * meas_h
+bound_h <- 1000
+
+dat_ffp <- dat_file %>%
+  mutate(
+    yyyy = year(TIMESTAMP_START),
+    mm = month(TIMESTAMP_START),
+    day = day(TIMESTAMP_START),
+    HH_UTC = hour(TIMESTAMP_START),
+    MM = minute(TIMESTAMP_START),
+    zm = meas_h,
+    d = d, 
+    #h = bound_h,
+    z0 = "-999",
+    u_mean = mean(WS_1_1_1, na.rm = TRUE),
+    L = (-((USTAR^3) * (TA_1_1_1 + 273)) / (0.4 * 9.8 * (H / (1.25 * 1004)))),
+    sigma_v = sqrt((u_mean*((-1.3*L + 0.1)^2))/100000),
+    u_star = USTAR,
+    wind_dir = WD_1_1_1,
+    test = zm/L
+  ) %>%
+  filter(test >= -15.5)%>%
+  filter(u_star > 0.2)%>%
+  select(yyyy, mm, day, HH_UTC, MM, zm, d, z0, u_mean, L, sigma_v, u_star, wind_dir)%>%
+  filter(across(everything(), ~ . != "NA"))%>%
+  filter(HH_UTC %in% 8:17)
+#%>%filter(yyyy %in% 2018:2021)
+
+dat_ffp$L = as.numeric(dat_ffp$L)
+dat_ffp$sigma_v = as.numeric(dat_ffp$sigma_v)
+dat_ffp$u_star = as.numeric(dat_ffp$u_star)
+dat_ffp$wind_dir = as.numeric(dat_ffp$wind_dir)
+
+#===============================================================================
+#============================ =============================
+#===============================================================================
+
+NW_dat_ffp <- dat_ffp%>%
+  filter(wind_dir %in% 270:360)
+
+write.csv(NW_dat_ffp, pathtoNWOutput, row.names = FALSE)
