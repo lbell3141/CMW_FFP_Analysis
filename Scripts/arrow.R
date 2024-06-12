8e558a3c56d686d7064ac144b9fe245d30d0be7d
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-06-12 16:25:01 -0700

    fixed summarize error. added output.

--- Scripts/arrow.R
+++ Scripts/arrow.R
@@ -68,18 +68,29 @@ bar_df_HH <- ag3_dataset |>
 
 #calculate half hourly wind speed (u_i_bar) and direction (theta_v_bar)
 #calculate half hourly st dev of lateral wind velocity (sigma_v)
+
 bar_df_HH <- bar_df_HH |>
   group_by(HH_group_ID) |>
   summarise(
     u_i_bar = (1 / n()) * sum(U_i),
     u_x_bar = (1 / n()) * sum(U_i * sin(theta_stand - pi)),
-    u_y_bar = (1 / n()) * sum(U_i * cos(theta_stand - pi)),
+    u_y_bar = (1 / n()) * sum(U_i * cos(theta_stand - pi)))|>
+  summarise(
     theta_v_bar = atan2(u_x_bar, u_y_bar) + pi,
     sigma_v_sqd = (1 / (n() - 1)) * sum((U_i * sin(theta_v_bar - theta_stand))^2),
-    sigma_v = sqrt(abs(sigma_v_sqd)))
+    sigma_v = sqrt(abs(sigma_v_sqd)))|>
+  to_arrow()
+
+#select variables of interest to write to csv
+voi_dataset <- bar_df_HH |>
+  select(TimeStamp, u_i_bar, theta_v_bar, sigma_v)
 
 #bind final product to Ameriflux CMW record
 #to_arrow > write_csv_arrow
+write_csv_arrow(
+  voi_dataset,
+  "./Data/High_Freq_Data/Processed/output_A.csv",
+  col_names = T)
 
 toc()
 
