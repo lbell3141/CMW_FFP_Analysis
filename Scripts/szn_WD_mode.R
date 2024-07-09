818ce355edf058e9b496e05082bb70ee23d67c3b
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-07-09 09:23:56 -0700

    gpp with mode WD arrows

--- Scripts/szn_WD_mode.R
+++ Scripts/szn_WD_mode.R
@@ -1,6 +1,8 @@
 #seasonal wind direction dominance/gpp plot
 library(cimir)
 library(dplyr)
+library(igraph)
+library(ggplot2)
 #load CM_dat from calc_directional_ffp.R
 #find mode w charaacter from cimir func
 compute_mode <- function(x) {
@@ -15,6 +17,27 @@ szn_dat <- CM_dat %>%
   summarize(comp_dir_mode = compute_mode(comp_dir),
             gpp = mean(GPP_PI))
 
+#convert compass directions to generic angles
+#mathematical conversion not working; define angles by hand
+#add pi/2 to start from compass 0 instead of standard coord plane 0
+direction_to_angle <- function(direction) {
+  directions <- c("N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW")
+  angles <- c(0+(pi/2), pi/8+(pi/2), pi/4+(pi/2), 3*pi/8+(pi/2), pi/2+(pi/2), 5*pi/8+(pi/2), 3*pi/4+(pi/2), 7*pi/8+(pi/2), pi+(pi/2), 9*pi/8+(pi/2), 5*pi/4+(pi/2), 11*pi/8+(pi/2), 3*pi/2+(pi/2), 13*pi/8+(pi/2), 7*pi/4+(pi/2), 15*pi/8+(pi/2))
+  angle <- angles[match(direction, directions)]
+  return(angle)
+}
+
+#calc arrow positions 
+szn_dat <- szn_dat %>%
+  mutate(angle = sapply(comp_dir_mode, direction_to_angle),
+         xend = mm + 0.5 * cos(angle),
+         yend = gpp + 0.5 * sin(angle))
+
+#make plot
+plot(szn_dat$mm, szn_dat$gpp, xlab = "Month", ylab = "Average GPP", pch = 16)
+#add arrows to plot
+arrows(x0 = szn_dat$mm, y0 = szn_dat$gpp, x1 = szn_dat$xend, y1 = szn_dat$yend,
+       length = 0.1, angle = 20, col = "black")
 
 
 
