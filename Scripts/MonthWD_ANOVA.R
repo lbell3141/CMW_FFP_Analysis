4e4dfae42b887eee605ca1a464b68fe8f537935e
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-09-25 18:11:09 -0700

    showing lack of spatial influence with linear models and new anova set

---
+++ Scripts/MonthWD_ANOVA.R
@@ -0,0 +1,146 @@
+#ANOVA for modeled and observed GPP data
+#GPP ~ WD + mm + WD*mm
+
+library(ggplot2)
+library(reshape2)
+
+dat_voi<- dat_file %>%
+  summarize(
+    yyyy = year(TIMESTAMP_START),
+    mm = month(TIMESTAMP_START),
+    doy = yday(TIMESTAMP_START),
+    day = day(TIMESTAMP_START),
+    HH_UTC = hour(TIMESTAMP_START),
+    MM = minute(TIMESTAMP_START),
+    wind_sp = WS_1_1_1,
+    temp_atmos = TA_1_1_1,
+    u_star = USTAR,
+    wind_dir = WD_1_1_1,
+    gpp = GPP_PI,
+    precip = P,
+    rel_h = RH_1_1_1,
+    VPD = RHtoVPD(dat_file$RH_1_1_1, dat_file$TA_1_1_1, dat_file$PA),
+    ppfd = PPFD_IN_PI_F,
+    le = LE,
+    swc = SWC_PI_1_1_A
+  ) %>%
+  filter(HH_UTC >= 8 & HH_UTC <= 17)
+
+#split into direction windows
+deg_int <- seq(45, 360, by = 45)
+deg_int_real <- deg_int
+split_dat <- split(dat_voi, cut(dat_voi$wind_dir, breaks = c(0,deg_int_real), include.lowest = F, labels = deg_int_real))
+for (i in seq_along(split_dat)) {
+  split_dat[[i]]$dir_group <- rep(names(split_dat)[i], nrow(split_dat[[i]]))
+}
+dir_dat <- do.call(rbind, split_dat)
+
+#predict gpp:
+
+model <- lm(gpp ~ swc + temp_atmos + rel_h + ppfd + wind_sp + precip +HH_UTC, data=dir_dat)
+#predict data based on modeled relationship
+modeled_gpp <- predict(model, dir_dat)
+#add to df
+dir_dat$modeled_gpp <- modeled_gpp
+#find difference bt real and modeled observations 
+anova_dat <- dir_dat%>%
+  filter(modeled_gpp > 0)%>%
+  select(mm, dir_group, gpp, modeled_gpp)%>%
+  mutate(mm = as.character(mm),
+         dir_group= as.character(dir_group))
+#some NAs causing plotting issues, so remove
+anova_dat<- na.omit(anova_dat)
+#anova test=====================================================================
+
+obs_formula <- as.formula(gpp ~ dir_group + mm + dir_group*mm)
+obs_result <- aov(obs_formula, data = anova_dat)
+summary(obs_result)
+
+mod_formula <- as.formula(modeled_gpp ~ dir_group + mm + dir_group*mm)
+mod_result <- aov(mod_formula, data = anova_dat)
+summary(mod_result)
+
+#residuals==========================
+#get residuals
+obs_residuals <- residuals(obs_result)
+mod_residuals <- residuals(mod_result)
+#residual variance
+obs_var <- var(obs_residuals)
+mod_var <- var(mod_residuals)
+obs_var
+mod_var
+#effect size (eta^2)==========================
+#anova value results (use summary)
+obs_table <- summary(obs_result)[[1]]
+mod_table <- summary(mod_result)[[1]]
+#obs eta^2
+obs_ss_total <- sum(obs_table[, "Sum Sq"])
+obs_eta_squared <- obs_table[1:3, "Sum Sq"] / obs_ss_total
+#mod eta^2
+mod_ss_total <- sum(mod_table[, "Sum Sq"])
+mod_eta_squared <- mod_table[1:3, "Sum Sq"] / mod_ss_total
+#results
+obs_eta_squared
+mod_eta_squared
+
+#visualizing variance==========================
+#fit values based on anova formula
+obs_fitted <- fitted(obs_result)
+mod_fitted <- fitted(mod_result)
+#create plot_df
+fitted_df <- data.frame(
+  data_type = rep(c("Observed", "Modeled"), each = nrow(anova_dat)),
+  anova_data = c(anova_dat$gpp, anova_dat$modeled_gpp),
+  fitted_data = c(obs_fitted, mod_fitted)
+)
+
+ggplot(fitted_df, aes(x = anova_data, y = fitted_data, color = data_type)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "black") +
+  labs(title = "", x = "Input GPP", y = "Fitted GPP") +
+  theme_minimal()
+
+#CV of residuals================================
+obs_res_sd <- sd(obs_residuals)
+obs_mean <- mean(anova_dat$gpp)
+obs_cv <- (obs_res_sd/obs_mean)*100
+mod_res_sd <- sd(mod_residuals)
+mod_mean <- mean(anova_dat$modeled_gpp)
+mod_cv <- (mod_res_sd/mod_mean)*100
+obs_cv
+mod_cv
+
+#combining info==================================
+#data for plot summary:
+obs_var
+mod_var
+obs_cv
+mod_cv
+#obs_eta_squared
+obs_eta_squared_dir_group <- obs_eta_squared[1]
+obs_eta_squared_mm <- obs_eta_squared[2]
+obs_eta_squared_combo <- obs_eta_squared[3]
+#mod_eta_squared
+mod_eta_squared_dir_group <- mod_eta_squared[1]
+mod_eta_squared_mm <- mod_eta_squared[2]
+mod_eta_squared_combo <- mod_eta_squared[3]
+
+
+#combine measures into a single df
+comparison_df <- data.frame(
+  Metric = c('eta_squared_dir_group', 'eta_squared_mm', 'eta_squared_combo','cv', 'variance'),
+  Observed = c(obs_eta_squared_dir_group, obs_eta_squared_mm,obs_eta_squared_combo,obs_cv, obs_var),
+  Model = c(mod_eta_squared_dir_group, mod_eta_squared_mm, mod_eta_squared_combo, mod_cv, mod_var)
+)
+#switch plot format for bar plotting
+comparison_df <- melt(comparison_df, id.vars = "Metric")
+
+#plotting
+ggplot(comparison_df, aes(x = variable, y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~ Metric, scales = "free") +
+  labs(title = "ANOVA Stat Comparison", 
+       y = "", x = "") +
+  theme_minimal() +
+  scale_fill_manual(values = c("Observed" = "#00BFC4", "Model" = "#F8766D")) +
+  theme(legend.title = element_blank())
--- Scripts/linear_models.R
+++ Scripts/linear_models.R
@@ -1,26 +1,37 @@
 #dir_dat_avg from driver_circles.R
 library(ggplot2)
 library(patchwork)
+library(cowplot)
+#renaming obj from driver_circles.R
 avg_flux_dat <- dir_dat_avg
 flux_dat <- dir_dat
 
+#making linear model equation + checking r^2
+#le excluded
 model <- lm(gpp ~ swc + temp_atmos + rel_h + ppfd + wind_sp + precip, data=flux_dat)
 r_squared <- summary(model)$r.squared
 
+#predict data based on modeled relationship
 modeled_gpp <- predict(model, flux_dat)
-
+#add to df
 flux_dat$modeled_gpp <- modeled_gpp
-
-flux_dat_dir <- flux_dat%>%
+#find difference bt real and modeled observations 
+flux_dat <- flux_dat%>%
   filter(modeled_gpp > 0)%>%
+  mutate(diff_gpp = modeled_gpp - gpp)
+#find average gpp value by wind direction (WD as factor to arrange in order)
+flux_dat_dir <- flux_dat%>%
   group_by(dir_group)%>%
   summarize(mod_avg_gpp = mean(modeled_gpp, na.rm = T),
-            real_avg_gpp = mean(gpp, na.rm = T))
+            real_avg_gpp = mean(gpp, na.rm = T),
+            diff_avg_gpp = mean(diff_gpp, na.rm = T))
 flux_dat_dir$dir_group <- factor(flux_dat_dir$dir_group, levels = seq(10, 350, 10))
 flux_dat_dir <- flux_dat_dir%>%
   arrange(dir_group)
 
-modeled_an_avg_gpp <- ggplot(flux_dat_dir) +  
+
+#plotting===================================
+gpp_difference_annual <- ggplot(flux_dat_dir) +  
   geom_hline(
     aes(yintercept = y), 
     data.frame(y = c(4, 5)), 
@@ -30,7 +41,7 @@ modeled_an_avg_gpp <- ggplot(flux_dat_dir) +
     aes(
       x = dir_group,
       y = 5, 
-      fill = mod_avg_gpp 
+      fill = diff_avg_gpp 
     ),
     position = "dodge2"
   ) +
@@ -45,7 +56,7 @@ modeled_an_avg_gpp <- ggplot(flux_dat_dir) +
     color = "white"
   ) + 
   coord_polar() +
-  scale_fill_viridis_c(option = "viridis", name = "") +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0, name = "")+
   theme(
     axis.title = element_blank(), 
     axis.ticks = element_blank(), 
@@ -61,52 +72,127 @@ modeled_an_avg_gpp <- ggplot(flux_dat_dir) +
     fill = guide_colorbar(barwidth = 8, barheight = 0.5)
   )
 
-real_an_avg_gpp <- ggplot(flux_dat_dir) +  
-  geom_hline(
-    aes(yintercept = y), 
-    data.frame(y = c(4, 5)), 
-    color = "lightgrey"
-  ) +
-  geom_col(
-    aes(
-      x = dir_group,
-      y = 5, 
-      fill = real_avg_gpp 
-    ),
-    position = "dodge2"
-  ) +
-  geom_segment(
-    aes(
-      x = dir_group,  
-      y = 5,
-      xend = dir_group, 
-      yend = 5
-    ),
-    linetype = "dashed",
-    color = "white"
-  ) + 
-  coord_polar() +
-  scale_fill_viridis_c(option = "viridis", name = "") +
-  theme(
-    axis.title = element_blank(), 
-    axis.ticks = element_blank(), 
-    axis.text.y = element_blank(), 
-    axis.text.x = element_blank(),  
-    legend.position = "bottom",
-    text = element_text(color = "gray12", family = "Bell MT"),
-    panel.background = element_rect(fill = "white", color = "white"),
-    panel.grid = element_blank(),
-    panel.grid.major.x = element_blank()
-  ) +
-  guides(
-    fill = guide_colorbar(barwidth = 8, barheight = 0.5)
-  )
-
-modeled_an_avg_gpp+ 
-  real_an_avg_gpp+ 
-  plot_annotation(title = "Modeled vs Real Average GPP")
+gpp_difference_annual+ 
+  plot_annotation(title = "Modeled - Observed Mean WD GPP")
 
 
 #===============================================================================
 #================================monthly modeling===============================
 #===============================================================================
+flux_dat <- dir_dat
+mm = factor(flux_dat$mm)
+mm_split_dat <- split(flux_dat, mm)
+
+#function to make a model; predict; find difference for a given month
+#same as above but with relative df, x
+mod_mm <- function(x){
+  model <- lm(gpp ~ swc + temp_atmos + rel_h + ppfd + wind_sp + precip + HH_UTC, data=x)
+  x$modeled_gpp <- predict(model, x)
+  x <- x %>%
+    filter(modeled_gpp > 0)%>%
+    mutate(diff_gpp = modeled_gpp - gpp)
+  dir_df <- x %>%
+    group_by(dir_group)%>%
+    summarize(mod_avg_gpp = mean(modeled_gpp, na.rm = T),
+              real_avg_gpp = mean(gpp, na.rm = T),
+              diff_avg_gpp = mean(diff_gpp, na.rm = T))
+  dir_df$dir_group <- factor(dir_df$dir_group, levels = seq(10, 350, 10))
+  dir_df <- dir_df %>% arrange(dir_group)
+  return(dir_df)
+}
+
+mm_mod_list <- lapply(mm_split_dat, mod_mm)
+
+all_diff_avg_gpp <- unlist(lapply(mm_mod_list, function(df) df$diff_avg_gpp))
+overall_min <- min(all_diff_avg_gpp, na.rm = TRUE)
+overall_max <- max(all_diff_avg_gpp, na.rm = TRUE)
+
+#plot monthly circles
+plot_gpp_difference <- function(df, month) {
+  ggplot(df) +  
+    geom_hline(
+      aes(yintercept = y), 
+      data.frame(y = c(4, 5)), 
+      color = "lightgrey"
+    ) +
+    geom_col(
+      aes(
+        x = dir_group,
+        y = 5, 
+        fill = diff_avg_gpp 
+      ),
+      position = "dodge2"
+    ) +
+    geom_segment(
+      aes(
+        x = dir_group,  
+        y = 5,
+        xend = dir_group, 
+        yend = 5
+      ),
+      linetype = "dashed",
+      color = "white"
+    ) + 
+    coord_polar() +
+    scale_fill_gradient2(low = "blue", 
+                         mid = "white", 
+                         high = "red", 
+                         midpoint = 0, 
+                         limits = c(overall_min, overall_max), 
+                         name = "GPP Difference (Modeled - Observed)") +
+    theme(
+      axis.title = element_blank(), 
+      axis.ticks = element_blank(), 
+      axis.text.y = element_blank(), 
+      axis.text.x = element_blank(),  
+      legend.position = "none",
+      text = element_text(color = "gray12", family = "Bell MT"),
+      panel.background = element_rect(fill = "white", color = "white"),
+      panel.grid = element_blank(),
+      panel.grid.major.x = element_blank()
+    ) +
+    labs(title = month)
+}
+
+#list of individual plots
+plot_list <- Map(function(df, month) plot_gpp_difference(df, month), mm_mod_list, month.name)
+
+final_plot <- wrap_plots(plot_list, ncol = 6) +
+  plot_layout(guides = "collect")&
+  theme(legend.position = "bottom")
+
+final_plot + 
+  plot_annotation(title = "Modeled - Observed Mean WD GPP by Month") + 
+  theme(
+    text = element_text(color = "gray12", family = "Bell MT")
+  )
+
+#===============================================================================
+#================================monthly modeling===============================
+#===============================================================================
+
+#collapse list
+#add month column using df position in list
+mm_mod_list <- lapply(seq_along(mm_mod_list), function(i) {
+  mm_mod_list[[i]]$month <- i
+  return(mm_mod_list[[i]])
+})
+
+full_frame <- do.call(rbind, mm_mod_list)
+
+all_real_avg_gpp <- unlist(lapply(mm_mod_list, function(df) df$real_avg_gpp))
+overall_min <- min(all_real_avg_gpp, na.rm = TRUE)
+overall_max <- max(all_real_avg_gpp, na.rm = TRUE)
+
+ggplot(full_frame, aes(x = as.factor(dir_group), y = as.factor(month), fill = real_avg_gpp)) +
+  geom_tile() +
+  scale_fill_viridis_c(option = "viridis", name = "Observed GPP", limits = c(overall_min, overall_max)) +
+  scale_y_discrete(labels = seq(1, 12, 1)) +
+  scale_x_discrete(labels = seq(10, 360, 10)) +
+  labs(title = "", x = "Direction", y = "Month", fill = "real_avg_gpp") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
