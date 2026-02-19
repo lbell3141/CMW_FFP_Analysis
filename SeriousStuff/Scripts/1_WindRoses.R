11c8ed0cca22ab2a9475640b41fcf7cab87f9654
Bell <lindseybell@arizona.edu>
2026-02-19 13:34:59 -0700

    [major] switch dat to PI obs only (no FLUXNET products)

--- SeriousStuff/Scripts/1_WindRoses.R
+++ SeriousStuff/Scripts/1_WindRoses.R
@@ -7,36 +7,36 @@ read_wind <- function(file, ws_col, wd_col, skip = 0) {
   read.csv(file, na.strings = "-9999", skip = skip) %>%
     mutate(TIMESTAMP_START = ymd_hm(as.character(TIMESTAMP_START))) %>%
     transmute(
-      wind_sp  = .data[[ws_col]],
-      wind_dir = .data[[wd_col]]
+      wind_sp  = as.numeric(.data[[ws_col]]),
+      wind_dir = as.numeric(.data[[wd_col]])
     ) %>%
     filter(!is.na(wind_sp), !is.na(wind_dir))
 }
 
 # read in data from all 4 sites
 cmw <- read_wind(
-  "./SeriousStuff/Data/AMF_US-CMW_BASE_HH_2-5.csv",
+  "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/CMW_FullFluxes.csv",
   ws_col = "WS_1_1_1",
   wd_col = "WD_1_1_1",
   skip = 2
 )
 
 srg <- read_wind(
-  "./SeriousStuff/Data/AMF_US-SRG_FLUXNET_FULLSET_HH_2008-2024_5-7.csv",
-  ws_col = "WS",
-  wd_col = "WD"
+  "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/SRG_FullFluxes.csv",
+  ws_col = "WS_1_1_1",
+  wd_col = "WD_1_1_1"
 )
 
 srm <- read_wind(
-  "./SeriousStuff/Data/AMF_US-SRM_FLUXNET_FULLSET_HH_2004-2024_4-7.csv",
-  ws_col = "WS",
-  wd_col = "WD"
+  "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/SRM_FullFluxes.csv",
+  ws_col = "WS_1_1_1",
+  wd_col = "WD_1_1_1"
 )
 
 wkg <- read_wind(
-  "./SeriousStuff/Data/AMF_US-Wkg_FLUXNET_FULLSET_HH_2004-2024_4-7.csv",
-  ws_col = "WS",
-  wd_col = "WD"
+  "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/WKG_FullFluxes.csv",
+  ws_col = "WS_1_1_1",
+  wd_col = "WD_1_1_1"
 )
 
 #combine frames together
@@ -44,8 +44,9 @@ all_wind <- bind_rows(cmw, srg, srm, wkg)
 
 max_ws <- ceiling(max(all_wind$wind_sp, na.rm = TRUE))
 
-# Define shared bins (tweak as you like)
-ws_breaks <- c(0, 2, 4, 8, 10, max_ws)
+# Define shared bins
+ws_breaks <- c(0, 2, 4, 6, 8, 10, max_ws)
+n_cols <- length(ws_breaks) - 1
 
 
 plot_windrose <- function(data, site, outfile, breaks) {
@@ -70,7 +71,7 @@ plot_windrose <- function(data, site, outfile, breaks) {
     width = 1.1,
     
     breaks = breaks,
-    cols = "jet",
+    cols = viridis(n_cols, option = "D", direction = 1),
     
     auto.text = TRUE,
     fontsize = 15,
@@ -87,7 +88,7 @@ plot_windrose <- function(data, site, outfile, breaks) {
 }
 
 
-plot_windrose(cmw, "CMW", "CMW_windrose_daytime.png", ws_breaks)
-plot_windrose(srg, "SRG", "SRG_windrose_daytime.png", ws_breaks)
-plot_windrose(srm, "SRM", "SRM_windrose_daytime.png", ws_breaks)
-plot_windrose(wkg, "Wkg", "Wkg_windrose_daytime.png", ws_breaks)
+plot_windrose(cmw, "CMW", "./SeriousStuff/Figures/Windroses/CMW_windrose_daytime.png", ws_breaks)
+plot_windrose(srg, "SRG", "./SeriousStuff/Figures/Windroses/SRG_windrose_daytime.png", ws_breaks)
+plot_windrose(srm, "SRM", "./SeriousStuff/Figures/Windroses/SRM_windrose_daytime.png", ws_breaks)
+plot_windrose(wkg, "Wkg", "./SeriousStuff/Figures/Windroses/Wkg_windrose_daytime.png", ws_breaks)
