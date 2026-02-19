979d7d5ce26f0d14b6dbb0cb9ecae384cc0159e8
Bell <lindseybell@arizona.edu>
2026-02-19 16:33:02 -0700

    [major] change data to PI fluxes for all site; change regression from ordinary to total least squares

--- SeriousStuff/Scripts/5_FluxRankRegression.R
+++ SeriousStuff/Scripts/5_FluxRankRegression.R
@@ -1,3 +1,4 @@
+library(mcr)
 library(dplyr)
 library(lubridate)
 library(tidyr)
@@ -9,10 +10,10 @@ library(patchwork)
 deg_int    <- seq(0, 360, by = 20)
 deg_labels <- seq(20, 360, by = 20)
 sites <- list(
-  CMW = "./SeriousStuff/Data/AMF_US-CMW_BASE_HH_2-5.csv",
-  SRM = "./SeriousStuff/Data/AMF_US-SRM_FLUXNET_FULLSET_HH_2004-2024_4-7.csv",
-  SRG = "./SeriousStuff/Data/AMF_US-SRG_FLUXNET_FULLSET_HH_2008-2024_5-7.csv",
-  WKG = "./SeriousStuff/Data/AMF_US-Wkg_FLUXNET_FULLSET_HH_2004-2024_4-7.csv"
+  CMW = "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/CMW_FullFluxes.csv",
+  SRM = "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/SRM_FullFluxes.csv",
+  SRG = "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/SRG_FullFluxes.csv",
+  WKG = "./SeriousStuff/Data/FluxData/PI_DATA/Full_PI_Data_by_Site/WKG_FullFluxes.csv"
 )
 
 # Helper function to process each site
@@ -23,11 +24,11 @@ process_site <- function(file, site_name, nee_col, gpp_col, reco_col, wind_col, 
       yyyy     = year(TIMESTAMP_START),
       mm       = month(TIMESTAMP_START),
       HH_UTC   = hour(TIMESTAMP_START),
-      nee      = !!sym(nee_col),
-      gpp      = !!sym(gpp_col),
-      reco     = !!sym(reco_col),
-      wind_dir = !!sym(wind_col)
-    ) %>%
+      nee      = as.numeric(!!sym(nee_col)),
+      gpp      = as.numeric(!!sym(gpp_col)),
+      reco     = as.numeric(!!sym(reco_col)),
+      wind_dir = as.numeric(!!sym(wind_col))
+    )%>%
     filter(HH_UTC >= 8 & HH_UTC <= 17) %>%
     drop_na()
   
@@ -68,9 +69,9 @@ process_site <- function(file, site_name, nee_col, gpp_col, reco_col, wind_col, 
 #pull site data with helper function
 site_plot_dfs <- list(
   CMW = process_site(sites$CMW, "CMW", "NEE_PI", "GPP_PI", "RECO_PI", "WD_1_1_1", skip = 2),
-  SRM = process_site(sites$SRM, "SRM", "NEE_VUT_REF", "GPP_DT_VUT_REF", "RECO_DT_VUT_REF", "WD"),
-  SRG = process_site(sites$SRG, "SRG", "NEE_VUT_REF", "GPP_DT_VUT_REF", "RECO_DT_VUT_REF", "WD"),
-  WKG = process_site(sites$WKG, "WKG", "NEE_VUT_REF", "GPP_DT_VUT_REF", "RECO_DT_VUT_REF", "WD")
+  SRM = process_site(sites$SRM, "SRM", "NEE", "GPP", "RECO", "WD_1_1_1"),
+  SRG = process_site(sites$SRG, "SRG", "NEE", "GPP", "RECO", "WD_1_1_1"),
+  WKG = process_site(sites$WKG, "WKG", "NEE", "GPP", "RECO", "WD_1_1_1")
 )
 
 all_plot_df <- bind_rows(site_plot_dfs)
@@ -82,12 +83,16 @@ all_plot_df$site <- factor(all_plot_df$site, levels = c("CMW","SRM","SRG","WKG")
 r2_df <- all_plot_df %>%
   group_by(site, group) %>%
   summarise(
-    r2 = summary(lm(y ~ x))$r.squared,
+    r2 = {
+      fit <- mcreg(x, y, method.reg = "Deming")
+      cor(x, y)^2
+    },
     .groups = "drop"
   ) %>%
   mutate(
     r2_label = paste0("R² = ", round(r2, 3))
   )
+
 
 make_density_plot <- function(df, group_name, y_label) {
   df_sub <- df %>% filter(group == group_name)
@@ -97,10 +102,26 @@ make_density_plot <- function(df, group_name, y_label) {
   
   ggplot(df_sub, aes(x = x, y = y)) +
     geom_density_2d_filled(aes(fill = after_stat(level)), alpha = 0.6) +
-    geom_smooth(method = "lm", se = FALSE,
-                color = "red",
-                linetype = "dashed") +
-    geom_text(
+    stat_smooth(method = "lm", se = FALSE, color = NA)+
+    geom_abline(
+      data = df_sub %>%
+        group_by(site) %>%
+        summarise(
+          slope = {
+            fit <- mcreg(x, y, method.reg = "Deming")
+            fit@para[2]
+          },
+          intercept = {
+            fit <- mcreg(x, y, method.reg = "Deming")
+            fit@para[1]
+          }
+        ),
+      aes(slope = slope, intercept = intercept),
+      color = "red",
+      linetype = "dashed",
+      inherit.aes = FALSE
+    )+
+  geom_text(
       data = r2_sub,
       aes(label = r2_label),
       x = -Inf, y = Inf,
@@ -109,15 +130,10 @@ make_density_plot <- function(df, group_name, y_label) {
       size = 5,
       color = "white"
     ) +
-    scale_x_continuous(
-      limits = c(1, 18),
-      breaks = c(1, 18)
-    ) +
-    scale_y_continuous(
-      limits = c(1, 18),
-      breaks = c(1, 18)
-    ) +
-    scale_fill_viridis_d(
+    scale_x_continuous(breaks = c(1, 18)) +
+    scale_y_continuous(breaks = c(1, 18)) +
+    coord_cartesian(xlim = c(1, 18), ylim = c(1, 18), expand = FALSE)+
+  scale_fill_viridis_d(
       name = "Point density",
       guide = guide_legend(
         label.position = "right",
@@ -131,10 +147,12 @@ make_density_plot <- function(df, group_name, y_label) {
     ) +
     facet_wrap(~ site, nrow = 1) +
     theme_minimal(base_size = 12) +
+    theme_minimal(base_size = 12) +
     theme(
       axis.text = element_text(size = 12),
       axis.ticks = element_line(),
-      panel.grid = element_blank()
+      panel.grid = element_blank(),
+      panel.spacing = unit(1.2, "lines")
     )
 }
 
